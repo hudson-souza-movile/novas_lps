684c06baddbaef9be65b626e2c839444e37b7fa4
hudsonmacedo <hudsonmacedo14@gmail.com>
2017-07-05 11:40:25 -0300

    changed content of page on data.js

--- vivo/educa/resources/js/data.js
+++ vivo/educa/resources/js/data.js
@@ -2,49 +2,49 @@ const app1 = new Moon({
 	el: "#app",
 	data: {
 		page_title:
-		"Conteúdo de Alexandre Rossi, o Dr. Pet!",
+		"Educação aproxima seus horizontes!",
 		
 		page_subtitle:
-		"As melhores dicas do maior especialista em comportamento animal no Brasil para pessoas que são apaixonadas por seus bichinhos de estimação.",
+		"O Vivo Educa tem mais de 200 cursos à sua disposição, classificados entre 8 completas categorias, para aprender no seu tempo e onde quiser.",
 		
 		cta1:
-		"Quero dicas do Dr. Pet!",
+		"Quero começar a aprender!",
 		
 		sec1_title:
-		"Saia do básico",
+		"Mais que aulinhas",
 		
 		card1_title:
-		"Dicas do melhor",
+		"Dicas de carreira",
 		
 		card1_text:
-		"Alexandre é formado em Zootecnia e mestre em Psicologia pela USP, com especialização em Comportamento Animal pela Universidade de Queensland - Austrália.",
+		"Tenha aulas preparatórias para entrevistas de emprego com os cursos de carreira do e melhore suas entrevistas de emprego.",
 		
 		card2_title:
-		"Múltiplos canais",
+		"Cozinha e gastronomia",
 		
 		card2_text:
-		"O Dicas Pet leva pra você informações sobre cães, gatos, adestramento e sobre a Estopinha. Escolha quantos canais quiser receber sem pagar a mais por isso.",
+		"Gostaria de saber cozinhar melhor ou melhorar o que você já sabe? Aqui você consegue aprender com cursos básicos e avançados.",
 		
 		card3_title:
-		"Gatos também!",
+		"Aprenda tecnologia",
 		
 		card3_text:
-		"Se seu pet é um gatinho você também tem muito pra ver com Dicas Pet. Todas as semanas temos conteúdo novo pra você saber cuidar melhor do seu felino querido!",
+		"Em pleno século 21 não se pode mais ficar sem conhecer o mínimo da tecnologia que nos cerca e entendê-las melhor.",
 		
 		sec2_title:
 		"Como funciona?",
 		
 		sec2_text:
-		"O Claro Pet pode ser assinado apenas por cliente Claro, aqui neste site ou enviando uma mensagem com a palavra “PET” para o número 738. Depois de assinar você vai ter 3 dias para experiementar gratuitamente e depois o custo é de apenas R$3,99/semana.",
+		"O Vivo Educa pode ser assinado aqui neste site ou no aplicativo. Assinando e baixando o aplicativo, você vai ter acesso a todos os cursos da nossa plataforma, exercícios, materiais de apoio, além de ter cursos ao vivo e poder acessar todos os vídeos offline usando seu smartphone ou tablet.",
 		
 		sec2_text2:
-		"Todo assinante recebe 1 MMS referente a categoria que selecionou (Adestramento, Cão, Gato e Estopinha). Lembrando que você pode receber quantas quiser, é só enviar a palavra “MAIS” para 738!",
+		"O valor do serviço é de R$ 4,99/semana ou de R$12,99/mês (desconto de 13%) mas você também tem a opção de acessar todas as aulas gratuitas pelo tempo até encontrar o curso que vai querer!",
 		
 		sec3_title:
-		"Quer receber tudo isso?",
+		"Quer aprender mais?",
 		
 		cta2:
-		"Quero dicas do Dr. Pet!",
+		"Quero começar a aprender!",
 	}
 });
 
